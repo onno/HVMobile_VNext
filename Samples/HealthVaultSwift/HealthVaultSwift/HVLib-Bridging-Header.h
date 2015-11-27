dc5a8195277b7e69811b382816d27c953f2b184a
Onno van Zinderen Bakker <onno@vanzinderenbakker.nl>
2015-11-27 11:22:52 +0100

    Added Swift test project and added a bridging header to HVLib.

---
+++ Samples/HealthVaultSwift/HealthVaultSwift/HVLib-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  HVLib-Bridging-Header.h
+//  HealthVaultSwift
+//
+//  Created by Onno van Zinderen Bakker on 27-11-15.
+//  Copyright © 2015 Onno van Zinderen Bakker. All rights reserved.
+//
+
+#ifndef HVLib_Bridging_Header_h
+#define HVLib_Bridging_Header_h
+
+#import "HVLib.h"
+
+#endif /* HVLib_Bridging_Header_h */
---
+++ Samples/HealthVaultSwift/HealthVaultSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthVaultSwift
+//
+//  Created by Onno van Zinderen Bakker on 26-11-15.
+//  Copyright © 2015 Onno van Zinderen Bakker. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
